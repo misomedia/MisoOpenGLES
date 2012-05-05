d7ca001ef9c5ef4774268af377e12d5e677f6f12
Ryan Hiroaki Tsukamoto <ryantsukamoto@Ryan-Tsukamotos-iMac.local>
2012-05-04 18:49:54 -0700

    ...

---
+++ MisoOpenGLES.framework/Versions/A/Headers/MisoOpenGLES.h
@@ -0,0 +1,13 @@
+//
+//  MisoOpenGLES.h
+//  MisoOpenGLES
+//
+//  Created by Ryan Tsukamoto on 5/4/12.
+//  Copyright (c) 2012 Miso Media. All rights reserved.
+//
+
+//this will be imported in other imported headers in the future...
+#import <OpenGLESTypes/OpenGLESTypes.h>
+
+//this too will be imported in other imported headers in the future...
+#import <Generics/Generics.h>
